4096afd7afabc7c2e38a007b98e976ac8a4069d8
Keira <McClellandKeira05@gmail.com>
2025-03-24 09:10:47 -0500

    added and tested login feature

--- app.py
+++ app.py
@@ -11,8 +11,10 @@
 """
 
 import os
+import os
 from sqlite3 import dbapi2 as sqlite3
-from flask import Flask, request, g, redirect, url_for, render_template, flash, render_template_string
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask_session import Session
 
 # create our little application :)
 app = Flask(__name__)
@@ -20,9 +22,15 @@ app = Flask(__name__)
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
     SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 def connect_db():
@@ -65,6 +73,8 @@ def close_db(error):
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect("/login")
     db = get_db()
     cur = db.execute('select title, category, text, id from entries order by id desc')
     category_col = db.execute('SELECT DISTINCT category FROM entries')
@@ -76,6 +86,8 @@ def show_entries():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
     db = get_db()
     db.execute('insert into entries (title, category, text) values (?, ?, ?)',
                [request.form['title'], request.form['category'], request.form['text']])
@@ -83,8 +95,6 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-# deletes an entry, supposed to work however id is not being passed through, however i am not sure why
-# getting entry.id just doesnt seem to work
 @app.route('/delete', methods=['POST'])
 def delete_entry():
     db = get_db()
@@ -111,6 +121,26 @@ def edit_post():
     db.commit()
     flash('post was updated.')
     return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 # filter the posts to what we specify
 @app.route('/filter', methods=['POST'])
